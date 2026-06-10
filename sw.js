8657be6d44f72e4e3bcae02c95f944cdf141abaf
Jason Stone <jasonstone@Jasons-MacBook-Pro.local>
2026-06-10 11:22:58 -0400

    v1.0 - maps card, Official links, location pins, search grouping

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'disney-dining-v4';
+const CACHE = 'disney-dining-v5';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
